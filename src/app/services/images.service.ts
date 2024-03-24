a45091069cea84c018d5f91fc21dded08868ed37
Renan Souza <renansouza0122@gmail.com>
2024-03-24 10:43:41 -0300

    images services updated

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -2,24 +2,37 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Photo } from '../models/photos.model';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImagesService {
   private apiKey = environment.apiKey;
-  private apiUrl = 'https://api.unsplash.com/';
+  private apiUrl = 'https://api.unsplash.com';
 
   constructor(private httpClient: HttpClient) {}
 
   getPhotos(pages: number) {
     return this.httpClient.get<Photo[]>(
       `${this.apiUrl}/photos/?client_id=${this.apiKey}&page=${pages}&per_page=21`
+    ).pipe(
+      catchError(error => {
+        console.error('Error fetching photos:', error);
+        return throwError(error);
+      })
     );
   }
+  
   getSearchPhoto(pages: number, value: string) {
     return this.httpClient.get<Photo[]>(
-      `${this.apiUrl}search/photos/?client_id=${this.apiKey}&page=${pages}&per_page=31&query=${value}`
+      `${this.apiUrl}/search/photos/?client_id=${this.apiKey}&page=${pages}&per_page=31&query=${value}`
+    ).pipe(
+      catchError(error => {
+        console.error('Error searching photos:', error);
+        return throwError(error);
+      })
     );
   }
 }
