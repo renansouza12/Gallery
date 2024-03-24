d23e0138a1136e74d1fde34301cebd37a4275ccf
Renan Souza <renansouza0122@gmail.com>
2024-03-24 10:49:33 -0300

    environment path updated

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Photo } from '../models/photos.model';
 import { catchError } from 'rxjs/operators';
