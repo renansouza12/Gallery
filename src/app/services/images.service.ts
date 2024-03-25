494981c66b66bdd7864cadd78ba5e094af3d6af8
RenanSouza <62895143+renansouza12@users.noreply.github.com>
2024-03-25 19:21:43 -0300

    images service changed

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -9,14 +9,14 @@ import { throwError } from 'rxjs';
   providedIn: 'root',
 })
 export class ImagesService {
-  private apiKey = environment.apiKey;
   private apiUrl = 'https://api.unsplash.com';
 
   constructor(private httpClient: HttpClient) {}
 
   getPhotos(pages: number) {
+    const apiKey = environment.apiKey; 
     return this.httpClient.get<Photo[]>(
-      `${this.apiUrl}/photos/?client_id=${this.apiKey}&page=${pages}&per_page=21`
+      `${this.apiUrl}/photos/?client_id=${apiKey}&page=${pages}&per_page=21`
     ).pipe(
       catchError(error => {
         console.error('Error fetching photos:', error);
@@ -26,8 +26,9 @@ export class ImagesService {
   }
   
   getSearchPhoto(pages: number, value: string) {
+    const apiKey = environment.apiKey; 
     return this.httpClient.get<Photo[]>(
-      `${this.apiUrl}/search/photos/?client_id=${this.apiKey}&page=${pages}&per_page=31&query=${value}`
+      `${this.apiUrl}/search/photos/?client_id=${apiKey}&page=${pages}&per_page=31&query=${value}`
     ).pipe(
       catchError(error => {
         console.error('Error searching photos:', error);
